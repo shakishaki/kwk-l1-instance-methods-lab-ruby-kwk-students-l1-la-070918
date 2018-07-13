693dbd4c5018a532e560bae7c410d7f9475e27bd
shakishaki <shaki.canchola@yahoo.com>
2018-07-13 21:30:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
 # dog.rb
+ class Dog 
+
+ attr_accessor :sound, :bark
+
+ def initialize (sound)
+   @sound = sound
+  
+ end
+
+ def bark
+   @bark = "woof"
+
+  
+end
+
+puts dog_one.bark
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,17 @@
 # person.rb
+class Person 
+
+ attr_accessor :sound, :bark
+
+ def initialize (sound)
+   @sound = sound
+  
+ end
+
+ def bark
+   @bark = "woof"
+
+  
+end
+
+puts dog_one.bark
